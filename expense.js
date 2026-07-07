c61c82434774b867ef28b1c131a98c310d26efff
Audrey Lee <audrey06lee05@gmail.com>
2026-07-07 12:37:24 +0100

    Add initial Expense class

---
+++ expense.js
@@ -0,0 +1,82 @@
+// This class represents one expense.
+class Expense {
+  #id;
+  #currency;
+  #amount;
+  #category;
+  #date;
+  #note;
+
+  constructor(id, currency, amount, category, date, note) {
+    this.#id = id;
+    this.currency = currency;
+    this.amount = amount;
+    this.category = category;
+    this.date = date;
+    this.note = note;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get amount() {
+    return this.#amount;
+  }
+  set amount(value) {
+    if (typeof value !== "number" || Number.isNaN(value)) {
+      throw new Error("Input type must be a number");
+    } else if (value <= 0) {
+      throw new Error("Amount has to be more than 0");
+    }
+    this.#amount = value;
+  }
+
+  get currency() {
+    return this.#currency;
+  }
+  set currency(value) {
+    if (!value) {
+      throw new Error("Currency cannot be empty");
+    } else if (typeof value !== "string") {
+      throw new Error("Input type invalid");
+    }
+    this.#currency = value;
+  }
+
+  get category() {
+    return this.#category;
+  }
+  set category(value) {
+    if (!value) {
+      throw new Error("Category cannot be empty");
+    } else if (typeof value !== "string") {
+      throw new Error("Invalid category entered");
+    }
+    this.#category = value;
+  }
+
+  get date() {
+    return this.#date;
+  }
+  set date(value) {
+    if (!value) {
+      throw new Error("Date cannot be empty");
+    } else if (typeof value !== "string") {
+      throw new Error("Invalid date entered");
+    }
+    this.#date = value;
+  }
+
+  get note() {
+    return this.#note;
+  }
+  set note(value) {
+    if (!value) {
+      value = "";
+    } else if (typeof value !== "string") {
+      throw new Error("Invalid note entered.");
+    }
+    this.#note = value;
+  }
+}
